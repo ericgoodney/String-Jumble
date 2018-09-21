9cf099ec3976fd0ff4ff959dd00776f662a49e87
ericgoodney <42844640+ericgoodney@users.noreply.github.com>
2018-09-21 13:04:55 -0400

    Updated from Brython Server: 9/21/2018 1:04:54 PM

--- stringjumble.py
+++ stringjumble.py
@@ -1,7 +1,7 @@
 """
 stringjumble.py
-Author: <your name>
-Credit: <sources>
+Author: Eric Goodney
+Credit: Peers and internet 
 
 Assignment:
 
@@ -25,3 +25,7 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+text = input("Please enter a string of text (the bigger the better): ") 
+print("You entered " +  text + " . Now jumble it")
+
+
