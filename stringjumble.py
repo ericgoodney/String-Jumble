d6d1d6afef6db30c8b7ca676f103a85b723df82b
ericgoodney <42844640+ericgoodney@users.noreply.github.com>
2018-09-24 12:51:06 -0400

    Updated from Brython Server: 9/24/2018 12:51:06 PM

--- stringjumble.py
+++ stringjumble.py
@@ -28,6 +28,12 @@ erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 text = input("Please enter a string of text (the bigger the better): ") 
 print("You entered " +  text + " . Now jumble it:")
 
+words = []
 
-print(text[::-1])
+def reverse(text): 
+    print(text[::-1])
+
+
+
+
 
