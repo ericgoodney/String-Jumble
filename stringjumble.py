595c7d70f476db2b13fd5e23bc047555e3cfc60f
ericgoodney <42844640+ericgoodney@users.noreply.github.com>
2018-09-24 13:00:43 -0400

    Updated from Brython Server: 9/24/2018 1:00:44 PM

--- stringjumble.py
+++ stringjumble.py
@@ -27,8 +27,13 @@ erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
 text = input("Please enter a string of text (the bigger the better): ") 
 print("You entered " +  text + " . Now jumble it:")
+
 word = []
 print(text[::-1])
+
+
+
+
 
 
 
