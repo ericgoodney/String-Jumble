8979dd93c6ae593a73f0ff82f2c19970899d0d2f
ericgoodney <42844640+ericgoodney@users.noreply.github.com>
2018-09-25 12:33:15 -0400

    Updated from Brython Server: 9/25/2018 12:33:15 PM

--- stringjumble.py
+++ stringjumble.py
@@ -1,7 +1,7 @@
 """
 stringjumble.py
 Author: Eric Goodney
-Credit: Peers and internet 
+Credit: Peers and internet(https://www.geeksforgeeks.org/reverse-words-given-string-python/)
 
 Assignment:
 
@@ -31,6 +31,31 @@ print("You entered " +  text + ". Now jumble it:")
 word = []
 print(text[::-1])
 
+# Function to reverse words of string 
+  
+def reverseWords(input): 
+      
+    # split words of string separated by space 
+    inputWords = input.split(" ") 
+  
+    # reverse list of words 
+    # suppose we have list of elements list = [1,2,3,4],  
+    # list[0]=1, list[1]=2 and index -1 represents 
+    # the last element list[-1]=4 ( equivalent to list[3]=4 ) 
+    # So, inputWords[-1::-1] here we have three arguments 
+    # first is -1 that means start from last element 
+    # second argument is empty that means move to end of list 
+    # third arguments is difference of steps 
+    inputWords=inputWords[-1::-1] 
+  
+    # now join words with space 
+    output = ' '.join(inputWords) 
+      
+    return output 
+  
+if __name__ == "__main__": 
+    input = text
+    print(reverse(Words(input)))
 
 
 
